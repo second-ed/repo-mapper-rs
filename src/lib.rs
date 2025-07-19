580b6a21279c6f7edd155dbdb966f08dcd509f77
ed cuss <edcussmusic@gmail.com>
2025-07-19 11:37:25 +0100

    feat: rough structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,31 @@
+use std::{path::Path, process::ExitCode};
+
 use pyo3::prelude::*;
 
-/// Formats the sum of two numbers as string.
+use crate::core::main;
+
 #[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
+fn py_main(scripts_root: String, readme_path: String) -> PyResult<i8> {
+    match main(scripts_root, Path::new(&readme_path)) {
+        ExitCode::SUCCESS => Ok(0),
+        ExitCode::FAILURE => Ok(1),
+        _ => Ok(-1),
+    }
 }
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn repo_mapper_rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(py_main, m)?)?;
     Ok(())
+}
+
+mod core {
+    use std::{path::Path, process::ExitCode};
+
+    pub fn main(scripts_root: String, readme_path: &Path) -> ExitCode {
+        println!("{}", &scripts_root);
+        dbg!(&readme_path);
+        ExitCode::SUCCESS
+    }
 }
