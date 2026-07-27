ba8832af5bffe8bad534e46821d4c91888e12d63
ed cuss <edcussmusic@gmail.com>
2026-07-27 21:38:34 +0100

    refactor: move cases to helpers

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -26,6 +26,7 @@ struct InputData {
     ignore_dirs: Vec<String>,
     ignore_hidden: bool,
     dirs_only: bool,
+    current_readme: String,
 }
 
 impl InputData {
@@ -36,11 +37,13 @@ impl InputData {
         ignore_dirs: Vec<&str>,
         ignore_hidden: bool,
         dirs_only: bool,
+        current_readme: &[&str],
     ) -> Self {
-        let readme_path = readme_path.to_string();
-        let gitignore_path = gitignore_path.to_string();
+        let readme_path = format!("{ROOT}/{readme_path}");
+        let gitignore_path = format!("{ROOT}/{gitignore_path}");
         let allowed_exts: Vec<String> = to_collection_of_type(allowed_exts);
         let ignore_dirs: Vec<String> = to_collection_of_type(ignore_dirs);
+        let current_readme = current_readme.join("\n");
 
         Self {
             readme_path,
@@ -49,6 +52,7 @@ impl InputData {
             ignore_dirs,
             ignore_hidden,
             dirs_only,
+            current_readme,
         }
     }
 }
@@ -59,114 +63,209 @@ struct ExpectedResult {
 }
 
 impl ExpectedResult {
-    fn new(ret_code: Result<RetCode, RetCode>, readme: &str) -> Self {
+    fn new(ret_code: Result<RetCode, RetCode>, readme: &[&str]) -> Self {
         Self {
             ret_code,
-            readme: readme.to_string(),
+            readme: readme.join("\n"),
         }
     }
 }
 
+fn given_valid_input_data_when_called_then_should_not_modify_the_readme(
+) -> (InputData, ExpectedResult) {
+    let current_readme = ["# Some readme\n\n\n# Repo map\n```\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n└── README.md\n\n(generated with repo-mapper-rs)\n::\n```"];
+    let expected_readme = ["# Some readme\n\n\n# Repo map\n```\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n└── README.md\n\n(generated with repo-mapper-rs)\n::\n```"];
+
+    (
+        InputData::new(
+            "README.md",
+            ".gitignore",
+            vec!["rs", "md", "toml"],
+            vec![".venv", "target"],
+            true,
+            false,
+            &current_readme,
+        ),
+        ExpectedResult::new(Ok(RetCode::NoModification), &expected_readme),
+    )
+}
+
+fn given_valid_input_data_when_called_with_dirs_only_then_should_not_modify_the_readme(
+) -> (InputData, ExpectedResult) {
+    let current_readme = ["# Some readme\n\n\n# Repo map\n```\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n└── README.md\n::\n```"];
+    let expected_readme = [
+        "# Some readme\n\n\n# Repo map\n```\n└── src\n\n(generated with repo-mapper-rs)\n::\n```",
+    ];
+
+    (
+        InputData::new(
+            "README.md",
+            ".gitignore",
+            vec!["rs", "md", "toml"],
+            vec![".venv", "target"],
+            true,
+            true,
+            &current_readme,
+        ),
+        ExpectedResult::new(Ok(RetCode::ModifiedReadme), &expected_readme),
+    )
+}
+
+fn given_empty_ignore_dirs_when_called_then_should_not_ignore_directories(
+) -> (InputData, ExpectedResult) {
+    let current_readme = ["# Some readme\n`\n\n# Repo map\n```\n├── .venv\n│   └── site-packages\n│       └── some_package.py\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n├── README.md\n└── scratch.py\n\n(generated with repo-mapper-rs)\n::\n```"];
+    let expected_readme = ["# Some readme\n`\n\n# Repo map\n```\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n├── README.md\n└── scratch.py\n\n(generated with repo-mapper-rs)\n::\n```"];
+
+    (
+        InputData::new(
+            "README.md",
+            ".gitignore",
+            vec!["rs", "md", "toml", "py"],
+            vec![],
+            true,
+            false,
+            &current_readme,
+        ),
+        ExpectedResult::new(Ok(RetCode::ModifiedReadme), &expected_readme),
+    )
+}
+
+fn given_ignored_directories_when_called_then_should_modify_the_readme(
+) -> (InputData, ExpectedResult) {
+    let current_readme = ["# Some readme\n"];
+    let expected_readme = ["# Some readme\n\n\n# Repo map\n```\n├── Cargo.toml\n├── README.md\n└── scratch.py\n\n(generated with repo-mapper-rs)\n::\n```",];
+
+    (
+        InputData::new(
+            "README.md",
+            ".gitignore",
+            vec![],
+            vec![".venv", "src"],
+            true,
+            false,
+            &current_readme,
+        ),
+        ExpectedResult::new(Ok(RetCode::ModifiedReadme), &expected_readme),
+    )
+}
+
+fn given_hidden_files_when_called_without_ignore_hidden_then_should_include_them(
+) -> (InputData, ExpectedResult) {
+    let current_readme = ["# Some readme\n"];
+    let expected_readme = ["# Some readme\n\n\n# Repo map\n```\n├── secrets\n│   └── .env\n├── .gitignore\n├── Cargo.toml\n├── README.md\n└── scratch.py\n\n(generated with repo-mapper-rs)\n::\n```"];
+
+    (
+        InputData::new(
+            "README.md",
+            ".gitignore",
+            vec![],
+            vec![".venv", "src"],
+            false,
+            false,
+            &current_readme,
+        ),
+        ExpectedResult::new(Ok(RetCode::ModifiedReadme), &expected_readme),
+    )
+}
+
+fn given_invalid_readme_filename_when_called_then_should_return_invalid_filename(
+) -> (InputData, ExpectedResult) {
+    let current_readme = ["# Some readme\n"];
+    let expected_readme = ["# Some readme\n"];
+
+    (
+        InputData::new(
+            "WRONG_README.txt",
+            ".gitignore",
+            vec![],
+            vec![],
+            true,
+            false,
+            &current_readme,
+        ),
+        ExpectedResult::new(Err(RetCode::InvalidFilename), &expected_readme),
+    )
+}
+
+fn given_missing_readme_when_called_then_should_return_failed_parsing_file(
+) -> (InputData, ExpectedResult) {
+    let current_readme = ["# Some readme\n"];
+    let expected_readme = ["# Some readme\n"];
+
+    (
+        InputData::new(
+            "docs/README.md",
+            ".gitignore",
+            vec![],
+            vec![],
+            true,
+            false,
+            &current_readme,
+        ),
+        ExpectedResult::new(Err(RetCode::FailedParsingFile), &expected_readme),
+    )
+}
+
+fn given_invalid_gitignore_filename_when_called_then_should_return_invalid_filename(
+) -> (InputData, ExpectedResult) {
+    let current_readme = ["# Some readme\n"];
+    let expected_readme = ["# Some readme\n"];
+
+    (
+        InputData::new(
+            "README.md",
+            ".gitdonotignore",
+            vec![],
+            vec![],
+            true,
+            false,
+            &current_readme,
+        ),
+        ExpectedResult::new(Err(RetCode::InvalidFilename), &expected_readme),
+    )
+}
+
 #[test_case(
-    InputData::new("fake/repo/root/README.md",
-    "fake/repo/root/.gitignore",
-    vec!["rs", "md", "toml"],
-    vec![".venv", "target"],
-    true, false),
-    "# Some readme\n\n\n# Repo map\n```\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n└── README.md\n\n(generated with repo-mapper-rs)\n::\n```",
-    ExpectedResult::new(Ok(RetCode::NoModification),
-    "# Some readme\n\n\n# Repo map\n```\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n└── README.md\n\n(generated with repo-mapper-rs)\n::\n```") ;
+    given_valid_input_data_when_called_then_should_not_modify_the_readme() ;
     "Ensure returns Ok(RetCode::NoModification)) when README is not modified"
 )]
 #[test_case(
-    InputData::new("fake/repo/root/README.md",
-    "fake/repo/root/.gitignore",
-    vec!["rs", "md", "toml"],
-    vec![".venv", "target"],
-    true, true),
-    "# Some readme\n\n\n# Repo map\n```\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n└── README.md\n::\n```",
-    ExpectedResult::new(Ok(RetCode::ModifiedReadme),
-    "# Some readme\n\n\n# Repo map\n```\n└── src\n\n(generated with repo-mapper-rs)\n::\n```" );
+    given_valid_input_data_when_called_with_dirs_only_then_should_not_modify_the_readme() ;
     "Ensure only shows directories if dirs_only is true"
 )]
 #[test_case(
-    InputData::new("fake/repo/root/README.md",
-    "fake/repo/root/.gitignore",
-    vec!["rs", "md", "toml", "py"],
-    vec![],
-    true, false),
-    "# Some readme\n`\n\n# Repo map\n```\n├── .venv\n│   └── site-packages\n│       └── some_package.py\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n├── README.md\n└── scratch.py\n\n(generated with repo-mapper-rs)\n::\n```",
-    ExpectedResult::new(Ok(RetCode::ModifiedReadme),
-    "# Some readme\n`\n\n# Repo map\n```\n├── src\n│   ├── lib.rs\n│   └── main.rs\n├── Cargo.toml\n├── README.md\n└── scratch.py\n\n(generated with repo-mapper-rs)\n::\n```") ;
+    given_empty_ignore_dirs_when_called_then_should_not_ignore_directories() ;
     "Ensure doesn't ignore directories if given empty vec"
 )]
 #[test_case(
-    InputData::new("fake/repo/root/README.md",
-    "fake/repo/root/.gitignore",
-    vec![],
-    vec![".venv", "src"],
-    true, false),
-    "# Some readme\n",
-    ExpectedResult::new(Ok(RetCode::ModifiedReadme),
-    "# Some readme\n\n\n# Repo map\n```\n├── Cargo.toml\n├── README.md\n└── scratch.py\n\n(generated with repo-mapper-rs)\n::\n```") ;
+    given_ignored_directories_when_called_then_should_modify_the_readme() ;
     "Ensure return Ok(RetCode::ModifiedReadme) if it modifies the README"
 )]
 #[test_case(
-    InputData::new("fake/repo/root/README.md",
-    "fake/repo/root/.gitignore",
-    vec![],
-    vec![".venv", "src"],
-    false, false),
-    "# Some readme\n",
-    ExpectedResult::new(Ok(RetCode::ModifiedReadme),
-    "# Some readme\n\n\n# Repo map\n```\n├── secrets\n│   └── .env\n├── .gitignore\n├── Cargo.toml\n├── README.md\n└── scratch.py\n\n(generated with repo-mapper-rs)\n::\n```") ;
+    given_hidden_files_when_called_without_ignore_hidden_then_should_include_them() ;
     "Ensure does not skip hidden file"
 )]
 #[test_case(
-    InputData::new("fake/repo/root/WRONG_README.txt",
-    "fake/repo/root/.gitignore",
-    vec![],
-    vec![],
-    true, false),
-    "# Some readme\n",
-    ExpectedResult::new(Err(RetCode::InvalidFilename),
-    "# Some readme\n" );
+    given_invalid_readme_filename_when_called_then_should_return_invalid_filename() ;
     "Ensure Err(RetCode::FailedParsingFile) if not pointed to a valid README"
 )]
 #[test_case(
-    InputData::new("fake/repo/root/docs/README.md",
-    "fake/repo/root/.gitignore",
-    vec![],
-    vec![],
-    true, false),
-    "# Some readme\n",
-    ExpectedResult::new(Err(RetCode::FailedParsingFile),
-    "# Some readme\n") ;
+    given_missing_readme_when_called_then_should_return_failed_parsing_file() ;
     "Ensure Err(FAILURE) if not pointed to an invalid file"
 )]
 #[test_case(
-    InputData::new("fake/repo/root/README.md",
-    "fake/repo/root/.gitdonotignore",
-    vec![],
-    vec![],
-    true, false),
-    "# Some readme\n",
-    ExpectedResult::new(Err(RetCode::InvalidFilename),
-    "# Some readme\n" );
+    given_invalid_gitignore_filename_when_called_then_should_return_invalid_filename() ;
     "Ensure Err(FAILURE) if not pointed to valid gitignore."
 )]
 #[allow(clippy::needless_pass_by_value)]
-#[allow(clippy::too_many_arguments)]
-fn test_modify_readme(
-    input_data: InputData,
-    current_readme: &str,
-    expected_result: ExpectedResult,
-) {
+fn test_modify_readme(test_data: (InputData, ExpectedResult)) {
+    let (input_data, expected_result) = test_data;
+
     let files = [
         ("src/main.rs", "let x = 1;"),
         ("src/lib.rs", "use std;"),
         ("Cargo.toml", ""),
-        ("README.md", current_readme),
+        ("README.md", &input_data.current_readme),
         (".gitignore", "target/"),
         ("target/some_build.rs", ""),
         (".venv/site-packages/some_package.py", ""),
@@ -190,7 +289,7 @@ fn test_modify_readme(
         input_data.dirs_only,
     );
 
-    let readme_pathbuf = PathBuf::from("fake/repo/root/README.md");
+    let readme_pathbuf = Path::new(ROOT).join("README.md");
 
     assert_eq!(exit_code, expected_result.ret_code);
     assert_eq!(
